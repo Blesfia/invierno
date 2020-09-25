0da31f63a65439fe923f1cc48dd6510a0123b7c4
David Sánchez <davidsanchez@MacBook-Pro-de-David.local>
2020-09-24 19:48:41 -0500

    improvee fastify plugin

--- packages/invierno-fastify/src/helpers/error-handler.ts
+++ packages/invierno-fastify/src/helpers/error-handler.ts
@@ -1,12 +1,12 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { HttpError } from 'invierno';
 
-export async function errorHandler(_: FastifyRequest, reply: FastifyReply, error: Error | HttpError): Promise<void> {
-  if (error?.isInviernoHttpError) {
-    return reply.status(error.statusCode).send({
-      data: error.data,
-      code: error.code,
-      message: error.message,
+export async function errorHandler(_: FastifyRequest, reply: FastifyReply, error: unknown): Promise<void> {
+  if ((error as HttpError)?.isInviernoHttpError) {
+    return reply.status((error as HttpError).statusCode).send({
+      data: (error as HttpError).data,
+      code: (error as HttpError).code,
+      message: (error as HttpError).message,
     });
   }
   throw error;
